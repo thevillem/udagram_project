3f23a9298359ed09c1bf3cc5bb4d0b39707fc9b1
thevillem <mike.villen@gmail.com>
2020-10-10 20:19:06 -0400

    Adding init script for instances.

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update -y
+apt-get install unzip awscli -y
+apt-get install apache2 -y
+systemctl start apache2.service
+cd /var/www/html
+aws s3 cp s3://udacity-demo-1/udacity.zip .
+unzip -o udacity.zip
